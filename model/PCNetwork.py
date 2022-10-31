092b3bdb642dddfbd45cff5c50e15e785a311ad3
Siyi Chen <siyich@umich.edu>
2022-10-31 19:17:41 -0400

    add LSTM simple model, update notes

--- model/PCNetwork.py
+++ model/PCNetwork.py
@@ -6,8 +6,10 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+# An RNN model
 class RNNModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, classification):
         super(RNNModel, self).__init__()
         # Hidden dimensions
         self.hidden_dim = hidden_dim
@@ -21,6 +23,8 @@ class RNNModel(nn.Module):
         # Readout layer
         self.fc = nn.Linear(hidden_dim, output_dim)
 
+        self.classification = classification
+
     def forward(self, x):
         # Initialize hidden state with zeros
         # (layer_dim, batch_size, hidden_dim)
@@ -33,6 +37,48 @@ class RNNModel(nn.Module):
         # Index hidden state of last time step
         # out.size() --> 100, 28, 10
         # out[:, -1, :] --> 100, 10 --> just want last time step hidden states! 
-        out = self.fc(out[:, -1, :]) 
+        if self.classification:
+            out = self.fc(out[:, -1, :]) 
+        # out.size() --> 100, 10
+        return out
+
+
+# An LSTM model 
+class LSTMModel(nn.Module):
+    def __init__(self, input_dim, hidden_dim, layer_dim, output_dim, classification):
+        super(LSTMModel, self).__init__()
+        # Hidden dimensions
+        self.hidden_dim = hidden_dim
+
+        # Number of hidden layers
+        self.layer_dim = layer_dim
+
+        # Building your LSTM
+        # batch_first=True causes input/output tensors to be of shape
+        # (batch_dim, seq_dim, feature_dim)
+        self.lstm = nn.LSTM(input_dim, hidden_dim, layer_dim, batch_first=True)
+
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+
+        self.classification = classification
+
+    def forward(self, x):
+        # Initialize hidden state with zeros
+        h0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # Initialize cell state
+        c0 = torch.zeros(self.layer_dim, x.size(0), self.hidden_dim).requires_grad_()
+
+        # 28 time steps
+        # We need to detach as we are doing truncated backpropagation through time (BPTT)
+        # If we don't, we'll backprop all the way to the start even after going through another batch
+        out, (hn, cn) = self.lstm(x, (h0.detach(), c0.detach()))
+
+        # Index hidden state of last time step
+        # out.size() --> 100, 28, 100
+        # out[:, -1, :] --> 100, 100 --> just want last time step hidden states! 
+        if self.classification:
+            out = self.fc(out[:, -1, :]) 
         # out.size() --> 100, 10
         return out
