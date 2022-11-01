08af5631b6aa75f9608c6f36698b8dfa420788b7
Siyi Chen <siyich@umich.edu>
2022-10-31 22:10:12 -0400

    add data dimension test result

--- process_data/moving_minst.py
+++ process_data/moving_minst.py
@@ -1,25 +1,27 @@
 from MovingMNIST import MovingMNIST
 import torch
 
-train_set = MovingMNIST(root='../dataset/moving_minst', train=True, preprocess=True)
-test_set = MovingMNIST(root='../dataset/moving_minst', train=False, preprocess=False)
+train_set = MovingMNIST(root='../dataset/moving_minst',
+                        train=True, preprocess=True)
+test_set = MovingMNIST(root='../dataset/moving_minst',
+                       train=False, preprocess=False)
 
 batch_size = 100
 
 train_loader = torch.utils.data.DataLoader(
-                 dataset=train_set,
-                 batch_size=batch_size,
-                 shuffle=True)
+    dataset=train_set,
+    batch_size=batch_size,
+    shuffle=True)
 test_loader = torch.utils.data.DataLoader(
-                dataset=test_set,
-                batch_size=batch_size,
-                shuffle=False)
+    dataset=test_set,
+    batch_size=batch_size,
+    shuffle=False)
 
 print('==>>> total trainning batch number: {}'.format(len(train_loader)))
 print('==>>> total testing batch number: {}'.format(len(test_loader)))
 
 for seq, seq_target in train_loader:
     print('--- Sample')
-    print('Input:  ', seq.shape)
-    print('Target: ', seq_target.shape)
+    print('Input:  ', seq.shape)  # torch.Size([100, 10, 64, 64])
+    print('Target: ', seq_target.shape)  # torch.Size([100, 10, 64, 64])
     break
